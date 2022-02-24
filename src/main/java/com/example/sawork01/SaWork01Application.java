fe7c6b296b71cee3136b3ec10403c4388a909cbb
saul-worseman <64306553+saul-worseman@users.noreply.github.com>
2022-02-24 21:56:13 +0800

    first commit

---
+++ src/main/java/com/example/sawork01/SaWork01Application.java
@@ -0,0 +1,13 @@
+package com.example.sawork01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaWork01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaWork01Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/sawork01/asciipanel/TileTransformer.java
@@ -0,0 +1,5 @@
+package com.example.sawork01.asciipanel;
+
+public interface TileTransformer {
+    public void transformTile(int x, int y, AsciiCharacterData data);
+}
